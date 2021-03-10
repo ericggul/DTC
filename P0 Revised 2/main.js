c4cd7e8f209eaa369a5281d9e320f0a2d07d1dba
ericggul <57379903+ericggul@users.noreply.github.com>
2021-03-10 10:31:12 +0900

    P0 Revised 2

---
+++ P0 Revised 2/main.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function(){
+	$("#Real1").click(function(){
+		$("#Real1").css("display","none");
+		$("#Real2").css("fill", "pink");
+		$('#header').css("height", "8vh");
+		$("#Real2").click(function(){
+			$("#Real2").css("display","none");
+			$("#Real3").css("fill", "red");
+			$('#header').css("height", "9vh");
+			$("#Real3").click(function(){
+				$("#Real3").css("display","none");
+				$('#header').css({"height": "12vh","text-decoration": "none"});
+			})
+		})
+	})
+
+	$(".cls-26").click(function(){
+		$("#Blackswan_modal").css("display", "flex");
+	})
+
+	$("#Blackswan_modal").click(function(){
+		$("#Blackswan_modal").css("display", "none");
+	})
+})
