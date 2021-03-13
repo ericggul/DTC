32350b0ec72d9c70031d0768174f638b9f148924
ericggul <57379903+ericggul@users.noreply.github.com>
2021-03-13 19:18:04 +0900

    Change

--- P0 Revised 2/main.js
+++ P0 Revised 2/main.js
@@ -3,13 +3,16 @@ $(document).ready(function(){
 	$("#Real1").click(function(){
 		$("#Real1").css("display","none");
 		$("#Real2").css("fill", "pink");
+		$("#expl1").css("display", "block");
 		$('#header').css("height", "8vh");
 		$("#Real2").click(function(){
 			$("#Real2").css("display","none");
 			$("#Real3").css("fill", "red");
+			$("#expl2").css("display", "block");
 			$('#header').css("height", "9vh");
 			$("#Real3").click(function(){
 				$("#Real3").css("display","none");
+				$("#expl3").css("display", "block");
 				$('#header').css({"height": "12vh","text-decoration": "none"});
 			})
 		})
