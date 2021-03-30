c764338c5608cb2a4e2df771b678288861b64b78
ericggul <57379903+ericggul@users.noreply.github.com>
2021-03-30 10:33:10 +0900

    Some modifications

---
+++ P2/Typewriter Practice/main.js
@@ -0,0 +1,20 @@
+var speed = 50;
+var text = 'An announcement: It is never percieved that we will be able to go back to the good old days';
+var i = 0;
+var blackCounter = 0;
+var target = document.getElementById('writer');
+
+const writer = () =>{
+    if ( i< text.length ) {
+        document.getElementById('writer').innerHTML += text.charAt(i);
+        i++;
+        if (text.charAt(i) == ' '){
+            setTimeout(writer, 100);
+        }
+        else{
+            setTimeout(writer, 50);
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', writer, false);
