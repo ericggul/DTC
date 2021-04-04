8a68a5bf5faf1df29deb0c8c70951447d514766d
ericggul <57379903+ericggul@users.noreply.github.com>
2021-04-04 21:48:03 +0900

    Born, LIve and Die

---
+++ P1/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+    for (let i = 1; i<13; i++){
+        var position =  $(`#P${i}`).offset();
+        var width = $(`#T${i}`).width();
+        var height = $(`#T${i}`).height();
+        $(`#T${i}`).offset({top: position.top-height/1.76, left: position.left - width/2});
+
+        //resizing
+        window.addEventListener('resize',()=>{
+            var position =  $(`#P${i}`).offset();
+            var width = $(`#T${i}`).width();
+            var height = $(`#T${i}`).height();
+            $(`#T${i}`).offset({top: position.top-height/1.76, left: position.left - width/2});
+        })
+
+        $(`#O${i}`).click(function() {
+            $(`#L${i}`).toggleClass('clicked');
+            $(`#T${i}`).toggleClass('visible');
+        })
+
+        $(`#T${i}`).click(function() {
+            $(`#L${i}`).toggleClass('clicked');
+            $(`#T${i}`).toggleClass('visible');
+        })
+        
+    }
+
+    $("#wrap_loader").click(function(){
+        console.log('clicked');
+        $(".wrapper").toggleClass("wrapclicked");
+    })
+})
