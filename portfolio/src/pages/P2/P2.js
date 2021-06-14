483db1d8229663faddd0b3c301259169e83c695b
ericggul <ericggul@gmail.com>
2021-06-14 17:47:05 +0900

    Hompage

--- portfolio/src/pages/P2/P2.js
+++ portfolio/src/pages/P2/P2.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import P2Img from '../../assets/P2Img.png';
+import styles from './P2.module.scss';
 
 function P2(){
     return(
-        <div>
+        <div className={styles.container}>
             <img src = {P2Img} />
+            <a href= {P2Img} download  >
+                <div className={styles.download}>
+                    Download
+                </div>
+            </a>
         </div>
     )
 }
