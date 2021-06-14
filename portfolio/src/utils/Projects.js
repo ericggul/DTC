9e31219c135a3b46e5ec20c0740b468ee02cd888
ericggul <ericggul@gmail.com>
2021-06-14 17:24:50 +0900

    Update

--- portfolio/src/utils/Projects.js
+++ portfolio/src/utils/Projects.js
@@ -1,12 +1,8 @@
 export const P0 = {
-    name: 'Born, Live and Die',
+    name: 'Identity',
     type: 'P0',
-    assign: 'Small Multiples',
-    introduction: `A Geometrical Axiom, such as "Orthocenter, Circumcenter, Gravity Points share collinearity.", are derived from our Accumulated Abstract Knowledge.
-    However, in order for us to understand the geometrical axiom, we should always interpret the abstract knowledge into the descriptive geometrical shapes.
-    The Euclidean lines and circles above offers us a bridge that links to the abstract world, but still it takes part within the descriptive images.
-    My purpose of this project was to create the new usage of these Euclidean images, by highlighting its descriptiveness rather than its abstractness.
-    Can Euclidean lines and circles themselves create an aesthetic impression?`,
+    assign: 'Self Introduction',
+    introduction: `Click on each rectangle to reveal myself.`,
     link: 'https://ericggul.github.io/DTC/Assignment%201/P0%20Revised%202/index.html'
 }
 
