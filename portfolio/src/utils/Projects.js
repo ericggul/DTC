08061adc3e32a1e879ed2c2d126e3a76c3de49f2
ericggul <ericggul@gmail.com>
2021-06-14 17:27:40 +0900

    Homepage update

--- portfolio/src/utils/Projects.js
+++ portfolio/src/utils/Projects.js
@@ -19,19 +19,15 @@ export const P1 = {
 }
 
 export const P2 = {
-    name: 'Born, Live and Die',
+    name: 'Liberty St. & Ford Ave.',
     type: 'P2',
-    assign: 'Small Multiples',
-    introduction: `A Geometrical Axiom, such as "Orthocenter, Circumcenter, Gravity Points share collinearity.", are derived from our Accumulated Abstract Knowledge.
-    However, in order for us to understand the geometrical axiom, we should always interpret the abstract knowledge into the descriptive geometrical shapes.
-    The Euclidean lines and circles above offers us a bridge that links to the abstract world, but still it takes part within the descriptive images.
-    My purpose of this project was to create the new usage of these Euclidean images, by highlighting its descriptiveness rather than its abstractness.
-    Can Euclidean lines and circles themselves create an aesthetic impression?`,
+    assign: 'Infographics',
+    introduction: ``,
     link: 'http://localhost:3000/P2'
 }
 
 export const P3 = {
-    name: 'Born, Live and Die',
+    name: '(Scent of a Tulip)',
     type: 'P3',
     assign: 'Small Multiples',
     introduction: `A Geometrical Axiom, such as "Orthocenter, Circumcenter, Gravity Points share collinearity.", are derived from our Accumulated Abstract Knowledge.
